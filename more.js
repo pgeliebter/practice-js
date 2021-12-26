c256d2a3b43765f31df800d9d0e41c630d3ca487
Pinny Geliebter <pgibralter@gmail.com>
2021-12-26 11:12:11 -0500

    event loop config

--- more.js
+++ more.js
@@ -1,2 +1,27 @@
-let promise = Promise.resolve(false)
-console.log(!promise)
+let events = []
+function runLoop() {
+  const interval = setInterval(() => {
+    console.log('Running next batch', events.length)
+    while (events.length > 0) {
+      const nextEvent = events.shift()
+      console.log(nextEvent)
+    }
+  }, 990)
+  setTimeout(() => {
+    clearInterval(interval)
+  }, 10000)
+}
+function queEvent(event) {
+  events.push(event)
+}
+let count = 1
+function pushCount() {
+  queEvent(count)
+  count++
+}
+
+setInterval(() => {
+  pushCount()
+}, 183)
+
+runLoop()
