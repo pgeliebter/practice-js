9c89645e9734444270198a39b3044ba3ce1f4946
Pinny Geliebter <pgibralter@gmail.com>
2021-12-26 09:59:17 -0500

    promises are always true

--- more.js
+++ more.js
@@ -1,12 +1,2 @@
-function tryAndCatch() {
-  try {
-    throw new Error('oh no')
-  } finally {
-  }
-  // catch (e) {
-  //   // do nothing
-  // }
-
-  return 'we got to the end'
-}
-console.log(tryAndCatch())
+let promise = Promise.resolve(false)
+console.log(!promise)
